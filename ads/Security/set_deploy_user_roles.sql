6509a44653086cc3be36b5f771d67ae407455b34
Jonathan Lee <jonjlee@gmail.com>
2024-11-12 11:46:03 -0800

    Move adhoc scripts to own folder outside of project

--- ads/Security/set_deploy_user_roles.sql
+++
@@ -1,7 +0,0 @@
-drop user if exists [deploy]
-create user [deploy] from login [deploy];
-exec sp_addRoleMember 'db_ddladmin', 'deploy';
-exec sp_addRoleMember 'db_datawriter', 'deploy';
-exec sp_addRoleMember 'db_datareader', 'deploy';
-GRANT VIEW DEFINITION TO [deploy];
-go

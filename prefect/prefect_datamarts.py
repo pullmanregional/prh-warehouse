36d3fa07110f4eebd026c7a93f9c29b5b1a59f80
Jonathan Lee <jonjlee@gmail.com>
2025-02-05 11:50:41 -0800

    Datamart ingest - use prefect importtools directly to load modules

--- prefect/prefect_datamarts.py
+++ prefect/prefect_datamarts.py
@@ -1,8 +1,22 @@
+"""
+This module / script executes the Prefect flows defined in DEPLOYMENTS.
+Each flow should be stored in an external git repository.
+
+This module is necessary because there is no public API to run a single execution
+of a flow currently. However, we still use Prefect utilities to clone the code
+and import the flow function.
+
+The main entry point of the script, datamart_ingest(), is not defined as a flow,
+so the flows are triggered as top level Prefect flows, not subflows,
+unless datamart_ingest() is called within a flow, like prefect_prw_ingest.py does.
+"""
+
 import asyncio
 import importlib
 import os
 import pathlib
 from prefect import flow
+from prefect.utilities import importtools
 from prefect.runner.storage import GitRepository
 from prefect_github import GitHubCredentials
 from prefect_util import ingest_shell_op
@@ -18,11 +32,14 @@ DEPLOYMENTS = [
         source=GitRepository(url="https://github.com/jonjlee-streamlit/prh-dash.git"),
         entrypoint="prefect/prh-dash-ingest.py:prh_dash_ingest",
     ).to_deployment(
-        name="prh-dash-ingest",
+        name="prw-finance-dash",
     )
 ]
 
 
+# -----------------------------------------
+# Flow execution helper
+# -----------------------------------------
 async def exec_deployment(deployment):
     """
     Accepts a Prefect deployment definition and executes a single run.
@@ -46,10 +63,7 @@ async def exec_deployment(deployment):
     module_name, function_name = deployment.entrypoint.split(":")
 
     # Convert relative python file to a module and then import it
-    module_path = repo_path / module_name
-    spec = importlib.util.spec_from_file_location(module_name, str(module_path))
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
+    module = importtools.load_script_as_module(str(repo_path / module_name))
 
     # Call target function
     flow_fn = getattr(module, function_name)

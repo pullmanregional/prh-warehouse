6569f15114650e9744b4ee6d86445b02d58d3743
Jonathan Lee <jonjlee@gmail.com>
2025-02-05 21:22:50 -0800

    Undo test await

--- prefect/prefect_datamarts.py
+++ prefect/prefect_datamarts.py
@@ -24,13 +24,9 @@ EXTERNAL_CODE_DIR = pathlib.Path.home() / ".prw" / "repos"
 # Datamart ingest flow definitions
 # -----------------------------------------
 DEPLOYMENTS = [
-    (
-        await flow.from_source(
-            source=GitRepository(
-                url="https://github.com/jonjlee-streamlit/prh-dash.git"
-            ),
-            entrypoint="prefect/prh-dash-ingest.py:prh_dash_ingest",
-        )
+    flow.from_source(
+        source=GitRepository(url="https://github.com/jonjlee-streamlit/prh-dash.git"),
+        entrypoint="prefect/prh-dash-ingest.py:prh_dash_ingest",
     ).to_deployment(
         name="prw-datamart-finance-dash",
     )

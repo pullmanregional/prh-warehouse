e1cda6559e2f23e91d2309445e82d823860b50aa
Jonathan Lee <jonjlee@gmail.com>
2025-02-05 20:56:00 -0800

    Make datamart ingest a module only, not top level script and update comments

--- prefect/prefect_datamarts.py
+++ prefect/prefect_datamarts.py
@@ -1,26 +1,22 @@
 """
-This module / script executes the Prefect flows defined in DEPLOYMENTS.
+This module executes the Prefect flows defined in DEPLOYMENTS.
 Each flow should be stored in an external git repository.
 
 This module is necessary because there is no public API to run a single execution
-of a flow currently. However, we still use Prefect utilities to clone the code
-and import the flow function.
+of a flow currently. For example, calling flow.serve() starts a persistent process.
+Instead, we use Prefect utilities to clone the code, import, and execute the flow
+function.
 
-The main entry point of the script, datamart_ingest(), is not defined as a flow,
-so the flows are triggered as top level Prefect flows, not subflows,
-unless datamart_ingest() is called within a flow, like prefect_prw_ingest.py does.
+The main entry point of the module, datamart_ingest(), is called by prefect_prw_ingest.py.
+The function is not defined as a flow. Instead the flows by it are the actual subflows.
 """
 
 import asyncio
-import importlib
 import os
 import pathlib
 from prefect import flow
 from prefect.utilities import importtools
 from prefect.runner.storage import GitRepository
-from prefect_github import GitHubCredentials
-from prefect_util import ingest_shell_op
-from prefect_aws import AwsCredentials
 
 EXTERNAL_CODE_DIR = pathlib.Path.home() / ".prw" / "repos"
 
@@ -49,7 +45,7 @@ async def exec_deployment(deployment):
     * Parse the entrypoint definition into the python file and function
     * Import the python file and call entrypoint function
     """
-    print(f"Running flow {deployment.name}")
+    print(f"Running datamart flow: {deployment.name}")
 
     # Use deployment.storage to clone the code repo
     deployment.storage.set_base_path(EXTERNAL_CODE_DIR)
@@ -76,7 +72,3 @@ async def exec_deployment(deployment):
 async def datamart_ingest():
     deployments = [exec_deployment(deployment) for deployment in DEPLOYMENTS]
     await asyncio.gather(*deployments)
-
-
-if __name__ == "__main__":
-    asyncio.run(datamart_ingest())

b59f42028cf4d5adefc1f9a9b7d99d5361222377
Jonathan Lee <jonjlee@gmail.com>
2025-03-17 06:16:06 -0700

    Dev ingest script

---
+++ bin/ingest-dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PRW_ENV=dev
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+cd "$SCRIPT_DIR"
+
+# Export data sources
+read -p "Caboodle connection (empty to skip): " PRH_SOURCES_EPIC_INPUT_CONN
+echo ""
+if [ -n "$PRH_SOURCES_EPIC_INPUT_CONN" ]; then
+    echo "Exporting sources"
+    cd ../../prw-exporter/sources/epic/prefect
+    echo $PRW_ENV
+    pwd
+    pipenv run prefect profile use dev
+    PRW_ENV=$PRW_ENV PRH_SOURCES_EPIC_INPUT_CONN="$PRH_SOURCES_EPIC_INPUT_CONN" pipenv run python flow.py
+fi
+
+# Run full ingest flow
+cd "$SCRIPT_DIR/../prefect"
+pwd
+pipenv run prefect profile use dev
+PRW_ENV=$PRW_ENV pipenv run python prw_ingest.py

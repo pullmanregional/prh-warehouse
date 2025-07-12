ba7e1992bb3a44c2953660e5cfdd993d1d80c9e5
Jonathan Lee <jonjlee@gmail.com>
2025-07-12 09:24:31 -0700

    Ingest patients table and use durablekey in place of calculation for prw_id

--- ingest/ingest_encounters.py
+++ ingest/ingest_encounters.py
@@ -1,19 +1,15 @@
 import os
 import logging
-import warnings
 import pandas as pd
-import json
-from typing import List
 from datetime import datetime
-from sqlmodel import Session, select, inspect
+from sqlmodel import Session, select
 from prw_common.model import prw_model, prw_id_model
-from util import prw_id_utils, prw_meta_utils
+from util import prw_meta_utils
 from prw_common.cli_utils import cli_parser
 from prw_common.db_utils import (
     TableData,
     get_db_connection,
     mask_conn_pw,
-    clear_tables,
     clear_tables_and_insert_data,
 )
 
@@ -31,33 +27,19 @@ logging.basicConfig(level=logging.INFO)
 # -------------------------------------------------------
 # Extract from Source Files
 # -------------------------------------------------------
-def sanity_check_files(encounters_file, mychart_file, allergy_file):
-    """
-    Executed once at the beginning of ingest to validate CSV file
-    meets basic requirements.
-    """
-    error = None
-    if not os.path.isfile(encounters_file):
-        error = f"ERROR: encounters file does not exist: {encounters_file}"
-    elif not os.path.isfile(mychart_file):
-        error = f"ERROR: mychart file does not exist: {mychart_file}"
-    elif not os.path.isfile(allergy_file):
-        error = f"ERROR: allergy file does not exist: {allergy_file}"
-    if error is not None:
-        print(error)
-
-    return error is None
-
-
 def read_mrn_to_prw_id_table(engine):
     """
-    Read existing ID to MRN mapping from the PRW ID DB
+    Read existing ID to MRN mapping and other details needed to calculate encounter fields from the PRW ID DB
     """
     with Session(engine) as session:
         results = session.exec(
-            select(prw_id_model.PrwId.prw_id, prw_id_model.PrwId.mrn)
+            select(
+                prw_id_model.PrwIdDetails.prw_id,
+                prw_id_model.PrwIdDetails.mrn,
+                prw_id_model.PrwIdDetails.dob,
+            )
         )
-        return pd.DataFrame(results, columns=["prw_id", "mrn"])
+        return pd.DataFrame(results, columns=["prw_id", "mrn", "dob"])
 
 
 def read_encounters(csv_file: str, mrn_to_prw_id_df: pd.DataFrame = None):
@@ -120,21 +102,6 @@ def read_encounters(csv_file: str, mrn_to_prw_id_df: pd.DataFrame = None):
     )
 
     # Retain columns needed for tranform and load
-    patients_df = df[
-        [
-            "mrn",
-            "name",
-            "sex",
-            "dob",
-            "address",
-            "city",
-            "state",
-            "zip",
-            "phone",
-            "email",
-            "pcp",
-        ]
-    ].copy()
     encounters_df = df[
         [
             "mrn",
@@ -156,16 +123,12 @@ def read_encounters(csv_file: str, mrn_to_prw_id_df: pd.DataFrame = None):
     # Fix data types
     # -------------------------------------------------------
     # Convert MRN to string
-    patients_df["mrn"] = patients_df["mrn"].astype(str)
     encounters_df["mrn"] = encounters_df["mrn"].astype(str)
 
     # Convert with_pcp to boolean (CSV might have 0/1 instead of True/False)
     encounters_df["with_pcp"] = encounters_df["with_pcp"].astype(bool)
 
     # Handle date/time formats
-    # BirthDate column is in YYYY-MM-DD format
-    patients_df["dob"] = pd.to_datetime(patients_df["dob"])
-
     # AppointmentDateKey is in YYYYMMDD format (e.g., 20240301)
     # AppointmentTimeOfDayKey is in HHMM 24-hour format (e.g., 1400)
     encounters_df["encounter_date"] = pd.to_datetime(
@@ -176,127 +139,27 @@ def read_encounters(csv_file: str, mrn_to_prw_id_df: pd.DataFrame = None):
     )
 
     # -------------------------------------------------------
-    # Add prw_id to patients and encounters
+    # Add prw_id to encounters
     # -------------------------------------------------------
-    # Drop duplicate patients based on 'mrn' and keep the first occurrence
-    patients_df = patients_df.drop_duplicates(subset=["mrn"], keep="first").copy()
-
-    # Calculate prw_id from MRN, and store translation table
-    patients_df = prw_id_utils.calc_prw_id(
-        patients_df,
-        src_id_col="mrn",
-        id_col="prw_id",
-        src_id_to_id_df=mrn_to_prw_id_df,
+    encounters_df = encounters_df.merge(
+        mrn_to_prw_id_df[["prw_id", "mrn"]], on="mrn", how="left"
     )
-    mrn_to_prw_id_df = patients_df[["mrn", "prw_id"]].copy()
-
-    # Convert encounter MRN to prw_id
-    encounters_df = encounters_df.merge(mrn_to_prw_id_df, on="mrn", how="left")
     encounters_df.drop(columns=["mrn"], inplace=True)
 
-    return patients_df, encounters_df, mrn_to_prw_id_df
-
-
-def read_allergy(allergy_file: str, mrn_to_prw_id_df: pd.DataFrame):
-    """
-    Read allergy file and add info into patients_df
-    """
-    logging.info(f"Reading {allergy_file}")
-    allergy_df = pd.read_csv(
-        allergy_file,
-        skiprows=1,
-        index_col=False,
-        names=[
-            "mrn",
-            "allergen_key",
-            "name",
-            "reaction",
-            "type",
-            "severity",
-        ],
-    )
-
-    # Group by MRN, and merge the name, reaction, type, and severity into an object, then convert to json string
-    allergy_df["mrn"] = allergy_df["mrn"].astype(str)
-    allergy_df = (
-        allergy_df.groupby("mrn")[["name", "reaction", "type", "severity"]]
-        .apply(lambda x: x.to_json(orient="records"))
-        .reset_index(name="allergy")
-    )
-
-    # Convert MRN to prw_id
-    allergy_df = allergy_df.merge(mrn_to_prw_id_df, on="mrn", how="right")
-    allergy_df.drop(columns=["mrn"], inplace=True)
-
-    return allergy_df
-
-
-def read_mychart_status(mychart_file: str, mrn_to_prw_id_df: pd.DataFrame):
-    """
-    Read mychart file and add info into patients_df
-    """
-    mychart_df = pd.read_csv(
-        mychart_file,
-        skiprows=1,
-        index_col=False,
-        names=["mrn", "mychart_status", "mychart_activation_date"],
-        dtype={"mrn": str, "mychart_status": str, "mychart_activation_date": str},
-    )
-
-    # Convert non null mychart_activation_date records to datetime
-    mychart_df["mychart_activation_date"] = mychart_df["mychart_activation_date"].apply(
-        lambda x: datetime.strptime(x, "%Y%m%d") if x is not None else None
-    )
-
-    # Drop duplicate MRNs and map to prw_id
-    mychart_df = mychart_df.drop_duplicates(subset=["mrn"], keep="first")
-    mychart_df = mychart_df.merge(mrn_to_prw_id_df, on="mrn", how="inner")
-    mychart_df.drop(columns=["mrn"], inplace=True)
-
-    return mychart_df
+    return encounters_df
 
 
 # -------------------------------------------------------
 # Transform
 # -------------------------------------------------------
-def calc_patient_age(patients_df: pd.DataFrame):
-    """
-    Calculate age and age_in_mo_under_3 from dob
-    """
-    # Calculate age and age in months (if <3yo) from dob
-    logging.info("Calculating patient ages")
-    now = pd.Timestamp.now()
-
-    # Get dob as datetime
-    dob_df = patients_df["dob"]
-
-    # Calculate age in years
-    # Adjust by 1 year if birthday hasn't occurred this year
-    patients_df["age"] = now.year - dob_df.dt.year
-    mask = (now.month < dob_df.dt.month) | (
-        (now.month == dob_df.dt.month) & (now.day < dob_df.dt.day)
-    )
-    patients_df.loc[mask, "age"] -= 1
-
-    # Calculate age in months, but only retain if under 3
-    patients_df["age_in_mo_under_3"] = (
-        (now.year - dob_df.dt.year) * 12
-        + (now.month - dob_df.dt.month)
-        - (now.day < dob_df.dt.day)
-    )
-    patients_df.loc[patients_df["age"] > 2, "age_in_mo_under_3"] = None
-    patients_df["age_in_mo_under_3"] = patients_df["age_in_mo_under_3"].astype("Int64")
-    return patients_df
-
-
-def calc_age_at_encounter(encounters_df: pd.DataFrame, patients_df: pd.DataFrame):
+def calc_age_at_encounter(encounters_df: pd.DataFrame, mrn_to_prw_id_df: pd.DataFrame):
     """
     Calculate encounter age and encounter_age_in_mo_under_3 based on encounter_date and patient's DOB
     """
     logging.info("Calculating patient ages at encounter")
     # Get each patient's DOB
     encounters_df = encounters_df.merge(
-        patients_df[["prw_id", "dob"]], on="prw_id", how="left"
+        mrn_to_prw_id_df[["prw_id", "dob"]], on="prw_id", how="left"
     )
 
     # Get dob and encounter_date as datetimes
@@ -359,32 +222,6 @@ def fix_missing_los_cpt(encounters_df: pd.DataFrame):
     return encounters_df
 
 
-def partition_phi(patients_df: pd.DataFrame):
-    """
-    Partition patients_df into PHI, which will be stored in ID DB, and non-PHI, which will be stored in the main DB
-    """
-    logging.info("Partitioning PHI")
-    PHI_COLUMNS = [
-        "mrn",
-        "name",
-        "dob",
-        "address",
-        "zip",
-        "phone",
-        "email",
-    ]
-    LOCATION_COLUMNS = [
-        "city",
-        "state",
-    ]
-    id_details_df = patients_df[["prw_id"] + PHI_COLUMNS + LOCATION_COLUMNS]
-
-    # Remove PHI columns from main dataset
-    patients_df = patients_df.drop(columns=PHI_COLUMNS).copy()
-
-    return patients_df, id_details_df
-
-
 # -------------------------------------------------------
 # Main entry point
 # -------------------------------------------------------
@@ -413,49 +250,30 @@ def main():
     id_output_conn = args.prwid if args.prwid.lower() != "none" else None
     drop_tables = args.drop
     logging.info(
-        f"Input: {in_path}, output: {mask_conn_pw(output_conn)}, id output: {mask_conn_pw(id_output_conn or 'None')}"
+        f"Input: {in_path}, output: {mask_conn_pw(output_conn)}, id output: {mask_conn_pw(id_output_conn)}"
     )
     logging.info(f"Drop tables before writing: {drop_tables}")
 
     # Input files
     encounters_file = os.path.join(in_path, "encounters.csv")
-    mychart_file = os.path.join(in_path, "mychart.csv")
-    allergy_file = os.path.join(in_path, "allergy.csv")
-
-    # Sanity check the input file
-    if not sanity_check_files(encounters_file, mychart_file, allergy_file):
-        logging.error("ERROR: input error (see above). Terminating.")
+    if not os.path.isfile(encounters_file):
+        logging.error(f"ERROR: encounters file does not exist: {encounters_file}")
         exit(1)
 
-    # If ID DB is specified, read existing ID mappings
-    prw_id_engine, mrn_to_prw_id_df = None, None
-    if id_output_conn:
-        prw_id_engine = get_db_connection(id_output_conn, echo=SHOW_SQL_IN_LOG)
-        if prw_id_engine is None:
-            logging.error("ERROR: cannot open ID DB (see above). Terminating.")
-            exit(1)
-        if inspect(prw_id_engine).has_table(prw_id_model.PrwId.__tablename__):
-            logging.info("Using existing MRN to PRW ID mappings")
-            mrn_to_prw_id_df = read_mrn_to_prw_id_table(prw_id_engine)
-        else:
-            logging.info("ID DB table does not exist, will generate new ID mappings")
+    # Read existing ID mappings
+    prw_id_engine = get_db_connection(id_output_conn, echo=SHOW_SQL_IN_LOG)
+    if prw_id_engine is None:
+        logging.error("ERROR: cannot open ID DB (see above). Terminating.")
+        exit(1)
+    mrn_to_prw_id_df = read_mrn_to_prw_id_table(prw_id_engine)
 
     # Read source file into memory
-    patients_df, encounters_df, mrn_to_prw_id_df = read_encounters(
-        encounters_file, mrn_to_prw_id_df
-    )
-    mychart_df = read_mychart_status(mychart_file, mrn_to_prw_id_df)
-    allergy_df = read_allergy(allergy_file, mrn_to_prw_id_df)
+    encounters_df = read_encounters(encounters_file, mrn_to_prw_id_df)
 
-    # Add allergy data to patients_df
-    patients_df = patients_df.merge(allergy_df, on="prw_id", how="left")
-
-    # Transform data: only data correction and partitioning of PHI into separate DB.
+    # Transform data: only data correction and handling PHI.
     # All other data transformations should be done by later flows in the pipeline.
-    patients_df = calc_patient_age(patients_df)
-    encounters_df = calc_age_at_encounter(encounters_df, patients_df)
+    encounters_df = calc_age_at_encounter(encounters_df, mrn_to_prw_id_df)
     encounters_df = fix_missing_los_cpt(encounters_df)
-    patients_df, id_details_df = partition_phi(patients_df)
 
     # Get connection to output DBs
     prw_engine = get_db_connection(output_conn, echo=SHOW_SQL_IN_LOG)
@@ -464,11 +282,10 @@ def main():
         exit(1)
     prw_session = Session(prw_engine)
 
-    # Create tables if they do not exist
+    # Drop tables so DDL is reissued if requested
     if drop_tables:
         logging.info("Dropping existing tables")
-        prw_model.PrwMetaModel.metadata.drop_all(prw_engine)
-        prw_model.PrwModel.metadata.drop_all(prw_engine)
+        prw_model.PrwEncounterOutpt.__table__.drop(prw_engine, checkfirst=True)
 
     logging.info("Creating tables")
     prw_model.PrwMetaModel.metadata.create_all(prw_engine)
@@ -478,9 +295,7 @@ def main():
     clear_tables_and_insert_data(
         prw_session,
         [
-            TableData(table=prw_model.PrwPatient, df=patients_df),
             TableData(table=prw_model.PrwEncounterOutpt, df=encounters_df),
-            TableData(table=prw_model.PrwMyChart, df=mychart_df),
         ],
     )
 
@@ -492,25 +307,6 @@ def main():
     prw_session.commit()
     prw_session.close()
     prw_engine.dispose()
-
-    # Write ID data to separate DB if provided
-    if prw_id_engine:
-        logging.info("Writing ID data")
-        id_df = id_details_df[["prw_id", "mrn"]]
-        prw_id_session = Session(prw_id_engine)
-        prw_id_model.PrwIdModel.metadata.create_all(prw_id_engine)
-        prw_id_model.PrwIdDetails.metadata.create_all(prw_id_engine)
-        clear_tables_and_insert_data(
-            prw_id_session,
-            [
-                TableData(table=prw_id_model.PrwId, df=id_df),
-                TableData(table=prw_id_model.PrwIdDetails, df=id_details_df),
-            ],
-        )
-        prw_id_session.commit()
-        prw_id_session.close()
-        prw_id_engine.dispose()
-
     logging.info("Done")
 
 
---
+++ ingest/ingest_patients.py
@@ -0,0 +1,342 @@
+import os
+import logging
+import warnings
+import pandas as pd
+import json
+from typing import List
+from datetime import datetime
+from sqlmodel import Session, select, inspect
+from prw_common.model import prw_model, prw_id_model
+from util import prw_id_utils, prw_meta_utils
+from prw_common.cli_utils import cli_parser
+from prw_common.db_utils import (
+    TableData,
+    get_db_connection,
+    mask_conn_pw,
+    clear_tables,
+    clear_tables_and_insert_data,
+)
+
+# Unique identifier for this ingest dataset
+DATASET_ID = "patients"
+
+# -------------------------------------------------------
+# Config
+# -------------------------------------------------------
+# Logging configuration
+SHOW_SQL_IN_LOG = False
+logging.basicConfig(level=logging.INFO)
+
+
+# -------------------------------------------------------
+# Extract from Source Files
+# -------------------------------------------------------
+def sanity_check_files(patients_file, mychart_file, allergy_file):
+    """
+    Executed once at the beginning of ingest to validate CSV file
+    meets basic requirements.
+    """
+    error = None
+    if not os.path.isfile(patients_file):
+        error = f"ERROR: patients file does not exist: {patients_file}"
+    elif not os.path.isfile(mychart_file):
+        error = f"ERROR: mychart file does not exist: {mychart_file}"
+    elif not os.path.isfile(allergy_file):
+        error = f"ERROR: allergy file does not exist: {allergy_file}"
+    if error is not None:
+        print(error)
+
+    return error is None
+
+
+def read_patients(csv_file: str):
+    # -------------------------------------------------------
+    # Extract data from CSV file
+    # -------------------------------------------------------
+    logging.info(f"Reading {csv_file}")
+    patients_df = pd.read_csv(
+        csv_file,
+        skiprows=1,
+        names=[
+            "prw_id",
+            "mrn",
+            "name",
+            "sex",
+            "dob",
+            "address",
+            "city",
+            "state",
+            "zip",
+            "phone",
+            "email",
+        ],
+        dtype={
+            "prw_id": str,
+            "mrn": str,
+            "name": str,
+            "sex": str,
+            "address": str,
+            "city": str,
+            "state": str,
+            "zip": str,
+            "phone": str,
+            "email": str,
+        },
+        parse_dates=["dob"],
+        index_col=False,
+    )
+    # Extract just prw_id -> MRN mapping into separate dataframe to persist
+    mrn_to_prw_id_df = patients_df[["mrn", "prw_id"]].copy()
+
+    return patients_df, mrn_to_prw_id_df
+
+
+def read_allergy(allergy_file: str, mrn_to_prw_id_df: pd.DataFrame):
+    """
+    Read allergy file and add info into patients_df
+    """
+    logging.info(f"Reading {allergy_file}")
+    allergy_df = pd.read_csv(
+        allergy_file,
+        skiprows=1,
+        index_col=False,
+        names=[
+            "mrn",
+            "allergen_key",
+            "name",
+            "reaction",
+            "type",
+            "severity",
+        ],
+    )
+
+    # Group by MRN, and merge the name, reaction, type, and severity into an object, then convert to json string
+    allergy_df["mrn"] = allergy_df["mrn"].astype(str)
+    allergy_df = (
+        allergy_df.groupby("mrn")[["name", "reaction", "type", "severity"]]
+        .apply(lambda x: x.to_json(orient="records"))
+        .reset_index(name="allergy")
+    )
+
+    # Convert MRN to prw_id
+    allergy_df = allergy_df.merge(mrn_to_prw_id_df, on="mrn", how="right")
+    allergy_df.drop(columns=["mrn"], inplace=True)
+
+    return allergy_df
+
+
+def read_mychart_status(mychart_file: str, mrn_to_prw_id_df: pd.DataFrame):
+    """
+    Read mychart file and add info into patients_df
+    """
+    mychart_df = pd.read_csv(
+        mychart_file,
+        skiprows=1,
+        index_col=False,
+        names=["mrn", "mychart_status", "mychart_activation_date"],
+        dtype={"mrn": str, "mychart_status": str, "mychart_activation_date": str},
+    )
+
+    # Convert non null mychart_activation_date records to datetime
+    mychart_df["mychart_activation_date"] = mychart_df["mychart_activation_date"].apply(
+        lambda x: datetime.strptime(x, "%Y%m%d") if x is not None else None
+    )
+
+    # Drop duplicate MRNs and map to prw_id
+    mychart_df = mychart_df.drop_duplicates(subset=["mrn"], keep="first")
+    mychart_df = mychart_df.merge(mrn_to_prw_id_df, on="mrn", how="inner")
+    mychart_df.drop(columns=["mrn"], inplace=True)
+
+    return mychart_df
+
+
+# -------------------------------------------------------
+# Transform
+# -------------------------------------------------------
+def drop_dup_mrn(patients_df: pd.DataFrame):
+    """
+    Drop duplicate MRNs, retain the occurrence with the greatest prw_id (ie DurableKey)
+    """
+    idx = patients_df.groupby("mrn")["prw_id"].idxmax()
+    patients_df = patients_df.loc[idx].reset_index(drop=True)
+    return patients_df
+
+
+def calc_patient_age(patients_df: pd.DataFrame):
+    """
+    Calculate age and age_in_mo_under_3 from dob
+    """
+    # Calculate age and age in months (if <3yo) from dob
+    logging.info("Calculating patient ages")
+    now = pd.Timestamp.now()
+
+    # Get dob as datetime
+    dob_df = patients_df["dob"]
+
+    # Calculate age in years
+    # Adjust by 1 year if birthday hasn't occurred this year
+    patients_df["age"] = now.year - dob_df.dt.year
+    mask = (now.month < dob_df.dt.month) | (
+        (now.month == dob_df.dt.month) & (now.day < dob_df.dt.day)
+    )
+    patients_df.loc[mask, "age"] -= 1
+
+    # Calculate age in months, but only retain if under 3
+    patients_df["age_in_mo_under_3"] = (
+        (now.year - dob_df.dt.year) * 12
+        + (now.month - dob_df.dt.month)
+        - (now.day < dob_df.dt.day)
+    )
+    patients_df.loc[patients_df["age"] > 2, "age_in_mo_under_3"] = None
+    patients_df["age_in_mo_under_3"] = patients_df["age_in_mo_under_3"].astype("Int64")
+    return patients_df
+
+
+def partition_phi(patients_df: pd.DataFrame):
+    """
+    Partition patients_df into PHI, which will be stored in ID DB, and non-PHI, which will be stored in the main DB
+    """
+    logging.info("Partitioning PHI")
+    PHI_COLUMNS = [
+        "mrn",
+        "name",
+        "dob",
+        "address",
+        "zip",
+        "phone",
+        "email",
+    ]
+    LOCATION_COLUMNS = [
+        "city",
+        "state",
+    ]
+    id_details_df = patients_df[["prw_id"] + PHI_COLUMNS + LOCATION_COLUMNS]
+
+    # Remove PHI columns from main dataset
+    patients_df = patients_df.drop(columns=PHI_COLUMNS).copy()
+
+    return patients_df, id_details_df
+
+
+# -------------------------------------------------------
+# Main entry point
+# -------------------------------------------------------
+def parse_arguments():
+    parser = cli_parser(
+        description="Ingest source data into PRH warehouse.",
+        require_prw=True,
+        require_prwid=True,
+        require_in=True,
+    )
+
+    # Add script-specific arguments
+    parser.add_argument(
+        "--drop",
+        action="store_true",
+        help="Drop and recreate all tables before ingesting data",
+    )
+    return parser.parse_args()
+
+
+def main():
+    # Load config from cmd line
+    args = parse_arguments()
+    in_path = args.input
+    output_conn = args.prw
+    id_output_conn = args.prwid
+    drop_tables = args.drop
+    logging.info(
+        f"Input: {in_path}, output: {mask_conn_pw(output_conn)}, id output: {mask_conn_pw(id_output_conn)}"
+    )
+    logging.info(f"Drop tables before writing: {drop_tables}")
+
+    # Input files
+    patients_file = os.path.join(in_path, "patients.csv")
+    mychart_file = os.path.join(in_path, "mychart.csv")
+    allergy_file = os.path.join(in_path, "allergy.csv")
+
+    # Sanity check the input file
+    if not sanity_check_files(patients_file, mychart_file, allergy_file):
+        logging.error("ERROR: input error (see above). Terminating.")
+        exit(1)
+
+    # Read source file into memory
+    patients_df, mrn_to_prw_id_df = read_patients(patients_file)
+    mychart_df = read_mychart_status(mychart_file, mrn_to_prw_id_df)
+    allergy_df = read_allergy(allergy_file, mrn_to_prw_id_df)
+
+    # Add allergy data to patients_df
+    patients_df = patients_df.merge(allergy_df, on="prw_id", how="left")
+
+    # Transform data: only data correction and partitioning of PHI into separate DB.
+    # All other data transformations should be done by later flows in the pipeline.
+    patients_df = drop_dup_mrn(patients_df)
+    patients_df = calc_patient_age(patients_df)
+    patients_df, id_details_df = partition_phi(patients_df)
+    id_df = id_details_df[["prw_id", "mrn"]]
+
+    # Get connection to output DBs
+    prw_engine = get_db_connection(output_conn, echo=SHOW_SQL_IN_LOG)
+    prw_id_engine = get_db_connection(id_output_conn, echo=SHOW_SQL_IN_LOG)
+    if prw_engine is None:
+        logging.error("ERROR: cannot open output DB (see above). Terminating.")
+        exit(1)
+    if prw_id_engine is None:
+        logging.error("ERROR: cannot open ID DB (see above). Terminating.")
+        exit(1)
+    prw_session = Session(prw_engine)
+    prw_id_session = Session(prw_id_engine)
+
+    # Drop tables so DDL is reissued if requested
+    if drop_tables:
+        logging.info("Dropping existing tables")
+        prw_model.PrwPatient.__table__.drop(prw_engine, checkfirst=True)
+        prw_model.PrwMyChart.__table__.drop(prw_engine, checkfirst=True)
+        prw_id_model.PrwId.__table__.drop(prw_id_engine, checkfirst=True)
+        prw_id_model.PrwIdDetails.__table__.drop(prw_id_engine, checkfirst=True)
+
+    # Create tables if they do not exist
+    logging.info("Creating tables")
+    prw_model.PrwMetaModel.metadata.create_all(prw_engine)
+    prw_model.PrwModel.metadata.create_all(prw_engine)
+    prw_id_model.PrwIdModel.metadata.create_all(prw_id_engine)
+    prw_id_model.PrwIdDetails.metadata.create_all(prw_id_engine)
+
+    # Write into DB
+    clear_tables_and_insert_data(
+        prw_session,
+        [
+            TableData(table=prw_model.PrwPatient, df=patients_df),
+            TableData(table=prw_model.PrwMyChart, df=mychart_df),
+        ],
+    )
+
+    # Update last ingest time and modified times for source data files
+    modified = {in_path: datetime.fromtimestamp(os.path.getmtime(in_path))}
+    prw_meta_utils.write_meta(prw_session, DATASET_ID, modified)
+
+    # Commit and clean up main DB
+    prw_session.commit()
+    prw_session.close()
+    prw_engine.dispose()
+
+    # Write ID data to separate DB
+    logging.info("Writing ID data")
+    clear_tables_and_insert_data(
+        prw_id_session,
+        [
+            TableData(table=prw_id_model.PrwId, df=id_df),
+            TableData(table=prw_id_model.PrwIdDetails, df=id_details_df),
+        ],
+    )
+
+    # Cleanup
+    prw_id_session.commit()
+    prw_id_session.close()
+    prw_id_engine.dispose()
+
+    logging.info("Done")
+
+
+if __name__ == "__main__":
+    main()

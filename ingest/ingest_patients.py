5f3d722f9ba9c1d160157b3369a6d380fd037c28
Jonathan Lee <jonjlee@gmail.com>
2025-07-13 12:30:20 -0700

    Add pcp field to patient ingest

--- ingest/ingest_patients.py
+++ ingest/ingest_patients.py
@@ -69,6 +69,7 @@ def read_patients(csv_file: str):
             "zip",
             "phone",
             "email",
+            "pcp",
         ],
         dtype={
             "prw_id": str,
@@ -81,6 +82,7 @@ def read_patients(csv_file: str):
             "zip": str,
             "phone": str,
             "email": str,
+            "pcp": str,
         },
         parse_dates=["dob"],
         index_col=False,
